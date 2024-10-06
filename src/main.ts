a2befaed3df8f4846cbe814a1fc977259e702c12
Gordon Sumit <gordon.sumit@rubicotech.in>
2024-10-06 23:20:38 +0530

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
-      .setVersion('1.0')
-      .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  app.enableCors({
-    origin: '*',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  });
+  // const config = new DocumentBuilder()
+  //     .setTitle('Cats example')
+  //     .setDescription('The cats API description')
+  //     .setVersion('1.0')
+  //     .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
+  //
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: ["GET", "POST", "PUT", "DELETE"],
+  // });
   await app.listen(3000);
 }
 bootstrap();
